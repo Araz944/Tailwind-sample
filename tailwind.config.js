51af3012637ebffabef87ec442f10124f05137b7
Araz944 <araz944memmedov@gmail.com>
2021-06-13 18:15:42 +0400

    added some custom screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      // write "npm run build:css" each time in the terminal when using or modifying these resolutions
+      // write "npm run build:css" each time in the terminal when using or modifying these resolutions (when finish the work, write "npm run prod" in the terminal to make minimized css file)
       xs320: "320px",
       xs360: "360px",
       xs400: "400px",
@@ -11,8 +11,11 @@ module.exports = {
       xs520: "520px",
       xs580: "580px",
       sm640: "640px",
-      md: "768px",
-      lg: "1024px",
+      md760: "760px",
+      md840: "840px",
+      md930: "930px",
+      lg1020: "1020px",
+
       xl: "1280px",
       "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
