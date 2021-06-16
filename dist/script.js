51c888e92f7173e826f6fdea449d81e5aa15317b
Araz944 <araz944memmedov@gmail.com>
2021-06-16 20:03:27 +0400

    toggle menu

---
+++ dist/script.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById("hamburgerMenu");
+
+function clickButton() {
+  const toggleClass = document.getElementById("navMenu");
+  toggleClass.classList.toggle("hidden");
+}
