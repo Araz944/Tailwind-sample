79d12c02ca6f2aa5eb07488ec6602a39a5924050
Araz944 <araz944memmedov@gmail.com>
2021-06-17 22:50:05 +0400

    added swiper js

--- dist/script.js
+++ dist/script.js
@@ -4,3 +4,41 @@ function clickButton() {
   const toggleClass = document.getElementById("navMenu");
   toggleClass.classList.toggle("hidden");
 }
+
+// Swiper.js
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+
+  // effect: "cube",
+  // cubeEffect: {
+  //   shadow: true,
+  //   slideShadows: true,
+  //   shadowOffset: 20,
+  //   shadowScale: 0.94,
+  // },
+  effect: "fade",
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  speed: 1000,
+  parallax: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
